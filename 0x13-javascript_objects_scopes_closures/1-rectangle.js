8da108ab6e261e8ba97a7d8c904efdb135a32c73
Kirera paul murithi <114237403+paul-murithi@users.noreply.github.com>
2024-03-12 10:07:57 +0300

    Add new files
    JavaScript object basics
    Object-oriented JavaScript
    Class - ES6
    super - ES6
    extends - ES6
    Object prototypes
    Inheritance in JavaScript
    Closures
    this/self
    Modern JS

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Empty class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
